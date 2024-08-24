3aae92d7ca8e638966f897536ced7d0e4a9a472b
saipraneeth1605 <saipraneeth1605@gmail.com>
2024-08-24 21:59:44 +0530

    LFS

---
+++ gui.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e7399b58cfd8849d49c76c002d5cbe1e84cbf75be61d8e7f7aaa84197798438
+size 4418
---
+++ train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec54b9319933a66c10c97fa285649ee489578121d1263560b1577ada8db23bf6
+size 3367
